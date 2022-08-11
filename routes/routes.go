df7a930540d1f0fef763cf09805c807746fd5086
Igna <istefanini@fleni.org.ar>
2022-08-11 03:19:53 -0300

    eliminar getPayment

--- routes/routes.go
+++ routes/routes.go
@@ -12,7 +12,6 @@ func CreateRoutes(r *gin.Engine) {
 	v1.Use()
 	{
 		v1.GET("/healthcheck", controllers.Healthcheck)
-		v1.GET("/payments", controllers.GetPayment)
 		v1.POST("/notificaction-mol-payment", controllers.PostPayment)
 	}
 }
